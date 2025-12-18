8b690ae6440d302bbe596fc402c34d491ca636a2
kirankumarmedisetty <44562652+kirankumarmedisetty@users.noreply.github.com>
2025-12-18 23:32:06 +0000

    deploy single jenkins server

--- Terraform_Project/variables.tf
+++ Terraform_Project/variables.tf
@@ -9,21 +9,23 @@ variable "servers" {
     key_name  = string
   }))
   default = {
-    ansible-master = {
+    jenkins-master = {
       instance_type = "t3.micro"
       ami_id        = "ami-00ca570c1b6d79f36" # Amazon Linux 2023 kernel-6.1 AMI (example)
       key_name = "codespace-key"
     }
-    ansible-slave-01 = {
-      instance_type = "t3.micro"
-      ami_id        = "ami-00ca570c1b6d79f36"
-      key_name = "codespace-key"
-    }
-    ansible-slave-02 = {
-      instance_type = "t3.micro"
-      ami_id        = "ami-00ca570c1b6d79f36"
-      key_name = "codespace-key"
-    }
   }
 }
+    #ansible-slave-01 = {
+    #  instance_type = "t3.micro"
+    #  ami_id        = "ami-00ca570c1b6d79f36"
+    #  key_name = "codespace-key"
+    #}
+    #ansible-slave-02 = {
+     # instance_type = "t3.micro"
+     # ami_id        = "ami-00ca570c1b6d79f36"
+     # key_name = "codespace-key"
+    #}
+  ##}
+##}
   
